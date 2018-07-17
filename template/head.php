af5296d5976e32c35e3e9f60776a6af4de7fd064
B74177 <paul.vialart@edf.fr>
2018-07-17 11:02:55 +0200

    Old version

--- template/head.php
+++ template/head.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<?php var_dump(getcwd().'/app/controller/Controller.php'); ?>
+TEST
 <head>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
